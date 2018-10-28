6be635f543be311492a3af61a036dc788b4f9654
wisemuji <wisemuji@gmail.com>
2018-10-29 03:56:07 +0900

    타이틀바

--- app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MainActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MainActivity.java
@@ -1,14 +1,20 @@
 package s2017s25.kr.hs.mirim.waterqualitynotice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // custom title bar
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
+
 
 
     }
--- app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/SplashActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/SplashActivity.java
@@ -2,13 +2,7 @@ package s2017s25.kr.hs.mirim.waterqualitynotice;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
