76dfbab01e71ec8c1bdd2dfbf5e4ef370961b873
wisemuji <wisemuji@gmail.com>
2018-10-29 03:10:30 +0900

    프로젝트 생성 및 스플레시 구현

---
+++ app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MapActivity.java
@@ -0,0 +1,15 @@
+package s2017s25.kr.hs.mirim.waterqualitynotice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+
+    }
+}
---
+++ app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/SplashActivity.java
@@ -0,0 +1,24 @@
+package s2017s25.kr.hs.mirim.waterqualitynotice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+
+}
