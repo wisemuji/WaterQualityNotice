a5d0eb6084055fc160de29d02562b349fa08e0f7
wisemuji <wisemuji@gmail.com>
2018-10-31 02:07:24 +0900

    화면 전환

--- app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MainActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MainActivity.java
@@ -1,12 +1,15 @@
 package s2017s25.kr.hs.mirim.waterqualitynotice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-
+    ImageButton btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +17,15 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
+
+        btnSearch=findViewById(R.id.btn_search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
