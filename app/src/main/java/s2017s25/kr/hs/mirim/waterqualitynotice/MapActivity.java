42680259b7d617358be84d92fa9fbfd22d01eb27
wisemuji <wisemuji@gmail.com>
2018-11-01 03:26:46 +0900

    구글 맵 검색기능 구현

--- app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MapActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/waterqualitynotice/MapActivity.java
@@ -2,13 +2,28 @@ package s2017s25.kr.hs.mirim.waterqualitynotice;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.location.Location;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -16,6 +31,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends Activity
         implements OnMapReadyCallback {
 
+    LinearLayout inputAddress;
+    Button btnCurrent;
+    RelativeLayout btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +47,49 @@ public class MapActivity extends Activity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        inputAddress=findViewById(R.id.input_address);
+        btnCurrent=findViewById(R.id.btn_current);
+        btnSearch=findViewById(R.id.btn_search);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment1);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                System.out.println( "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                System.out.println( "An error occurred: " + status);
+            }
+        });
+
+
     }
 
     @Override
     public void onMapReady(final GoogleMap map) {
+        LatLng location = new LatLng(37.56, 126.97); //서울
+        String title = "";
+        String desc = "";
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+        addMarker(title, desc, location, map);
+    }
+
+    public void addMarker(String title, String desc, LatLng location, final GoogleMap map){
 
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
+        markerOptions.position(location);
+        markerOptions.title(title);
+        markerOptions.snippet(desc);
         map.addMarker(markerOptions);
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.moveCamera(CameraUpdateFactory.newLatLng(location));
         map.animateCamera(CameraUpdateFactory.zoomTo(10));
+
     }
 }
